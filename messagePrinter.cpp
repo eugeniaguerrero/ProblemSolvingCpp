6e2f3dbd3c3b7966da29f2d5489b296c2d40ef62
Eugenia Guerrero <angeleuge95@gmail.com>
2017-09-15 18:44:52 -0400

    adds programming project 7 of chapter 1

---
+++ messagePrinter.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "*****************************************************\n";
+    cout << "                                                    \n";
+
+    cout << "     C C C          S S S S     !!\n";
+    cout << "   C               S       S    !!\n";
+    cout << "  C               S             !!\n";
+    cout << "  C               S             !!\n";
+    cout << "  C                S S S S      !!\n";
+    cout << "  C                       S     !!\n";
+    cout << "   C                       S    !!\n";
+    cout << "    C             S       S       \n";
+    cout << "      C C C        S S S S      OO\n";
+
+    cout << "                                                    \n";
+    cout << "*****************************************************\n";
+
+    cout << "Computer Science is cool stuff!";
+
+    return 0;
+
+
+
+}
